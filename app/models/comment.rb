718cd144e7acf13db38d944cfe2fe3a725835194
ShirinHasan13 <shirin10@chicagobooth.edu>
2024-07-28 02:17:48 +0000

    d

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,15 +11,23 @@
 #
 
 class Comment < ApplicationRecord
-  validates(:commenter, { :presence => true })
-
   # Association accessor methods to define:
   
   ## Direct associations
 
   # Comment#commenter: returns a row from the users table associated to this comment by the author_id column
+  belongs_to :commenter, :class_name => "User", :foreign_key => "author_id", :optional => true
 
   # Comment#photo: returns a row from the photos table associated to this comment by the photo_id column
+  def photo
+    my_photo_id = self.photo_id
+
+    matching_photos = Photo.where({ :id => my_photo_id })
+
+    the_photo = matching_photos.at(0)
+
+    return the_photo
+  end
 
   def commenter
     my_author_id = self.author_id
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,59 +1,21 @@
-# == Schema Information
-#
-# Table name: photos
-#
-#  id             :integer          not null, primary key
-#  caption        :text
-#  comments_count :integer
-#  image          :string
-#  likes_count    :integer
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
-#  owner_id       :integer
-#
-
 class Photo < ApplicationRecord
-  validates(:poster, { :presence => true })
-
   # Association accessor methods to define:
   
   ## Direct associations
 
   # Photo#poster: returns a row from the users table associated to this photo by the owner_id column
+  belongs_to :poster, :class_name => "User", :foreign_key => "owner_id", :optional => true
 
   # Photo#comments: returns rows from the comments table associated to this photo by the photo_id column
+  has_many :comments, :class_name => "Comment", :foreign_key => "photo_id"
 
   # Photo#likes: returns rows from the likes table associated to this photo by the photo_id column
-
-  ## Indirect associations
+  has_many :likes, :class_name => "Like", :foreign_key => "photo_id"
 
   # Photo#fans: returns rows from the users table associated to this photo through its likes
+  has_many :fans, :through => :likes, :source => :fan
 
-  def poster
-    my_owner_id = self.owner_id
-
-    matching_users = User.where({ :id => my_owner_id })
-
-    the_user = matching_users.at(0)
-
-    return the_user
-  end
-
-  def comments
-    my_id = self.id
-
-    matching_comments = Comment.where({ :photo_id => self.id })
-
-    return matching_comments
-  end
-
-  def likes
-    my_id = self.id
-
-    matching_likes = Like.where({ :photo_id => self.id })
-
-    return matching_likes
-  end
+  ## Indirect associations
 
   def fans
     my_likes = self.likes
